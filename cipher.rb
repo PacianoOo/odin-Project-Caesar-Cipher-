1d765455d55245927eea0fb6c711d7771f5da1f8
pacianoseth <feintimg@gmail.com>
2023-05-29 01:00:18 +0800

    implemented my solution, need to optimize code for further efficiency

--- cipher.rb
+++ cipher.rb
@@ -1,24 +1,37 @@
 
-def cipher(string)
+def cipher(string, shift_factor)
     #store each element of the string into the new_string array
     #and convert the elements into its ascii value
     new_string = []
     string.chars.each do |char|
-        ascii_value = char.ord
-        new_string.push(ascii_value)
+        new_string.push(char.ord)
     end
     #check if each element is an alphabet uppercase or lowercase
     modified_string = []
     new_string.map do |number|
-        if number.between?(65,90)
-            modified_string.push(number)
+        if number.between?(65,90) || number.between?(97,122)
+            modified_string.push(number + shift_factor)
         else 
             modified_string.push(number.chr)
         end
     end
-    puts new_string
-    puts modified_string.inspect
-end
 
-cipher("Seth!")
+    #deduct 26 if element in modified_string is greater than 91 || 122
+    final_string = []
+    modified_string.map do |number|
+        if  number.is_a?(Numeric) && number > 90 && number < 97 || number.is_a?(Numeric) && number > 122
+            final_string.push(number - 26)
+        else
+            final_string.push(number)
+        end
+    end
+    #print out final_string array with .chr
+
+    puts new_string.inspect
+    puts modified_string.inspect
+    puts final_string.inspect
+    puts final_string.map {|number| number.chr}.join('')
+
+end
+cipher("Seth!!", 5)
 
